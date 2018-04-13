9fd383ed20853b0e1760c63a952a7a0243256176
Lan Fei <husano896@hotmail.com>
2018-04-13 11:59:13 +0800

    A

---
+++ XMLanalysis/ParkingDB.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenData;
+namespace XMLanalysis
+
+{
+    public class ParkingDB : MGenericsDB<桃園公共自行車即時服務資料>
+    {
+        public void InsertData(桃園公共自行車即時服務資料 item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<桃園公共自行車即時服務資料> QueryData(string searchColumn, string searchName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShowData(List<桃園公共自行車即時服務資料> list)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<桃園公共自行車即時服務資料> Xml_Load()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
