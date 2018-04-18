6a09656b565520971c68359534c791c34b7424a8
YuSyuan <YuSyua@com>
2018-04-18 12:55:20 +0800

    add xml

---
+++ XMLanalysis/App_Data/dbo.藥局.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[藥局] (
+    [Id]   INT            NOT NULL,
+    [機構名稱] NVARCHAR (MAX) NULL,
+    [機構狀態] NVARCHAR (MAX) NULL,
+    [地址] NVARCHAR (MAX) NULL,
+    [電話] NVARCHAR (MAX) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
