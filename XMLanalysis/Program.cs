606f51fe02024676b83c901f9bdb672b4c5a52e4
dell <dell@mail>
2018-04-19 18:48:35 +0800

    combine YuSyuan1208

--- XMLanalysis/Program.cs
+++ XMLanalysis/Program.cs
@@ -8,25 +8,28 @@ namespace XMLanalysis   {
      
     class Program{
         static void Main(string[] args){
-            
-            MGenericsDB<Pharmacy_03> mPharmacy_03 = new PharmacyTable_03();
+
+
+            //-------------------------------------------------------------ShengYaood
+            MGenericsDB<FarmTran> mFarm = new FarmTranTable();
             /*
             var nodeList = mFarm.Xml_Load();
             nodeList.ForEach(item =>
             {
                 mFarm.InsertData(item);
             });*/
-            //mFarm.ShowData(mFarm.QueryData("作物名稱", "椰子"));
-            //var farm = new FarmTran();
-            //farm.transactionDate = "107.4.13";
-            //mFarm.UpdateData(1,farm);
-            //mFarm.ShowData(mFarm.QueryData("交易日期", "107.4.13"));
-            //Console.ReadKey();
-            //Console.Clear();
-            //mFarm.DeleteData("交易日期", "107.4.13");
-            //mFarm.ShowData(mFarm.QueryData("交易日期", "107.4.13"));
-            //Console.ReadKey();
- //-------------------------------------------------------------           
+            mFarm.ShowData(mFarm.QueryData("作物名稱", "椰子"));
+            var farm = new FarmTran();
+            farm.transactionDate = "107.4.13";
+            mFarm.UpdateData(1,farm);
+            mFarm.ShowData(mFarm.QueryData("交易日期", "107.4.13"));
+            Console.ReadKey();
+            Console.Clear();
+            mFarm.DeleteData("交易日期", "107.4.13");
+            mFarm.ShowData(mFarm.QueryData("交易日期", "107.4.13"));
+            Console.ReadKey();
+            //-------------------------------------------------------------YuSyuan1208     
+            MGenericsDB<Pharmacy_03> mPharmacy_03 = new PharmacyTable_03();
             /*var nodeList = mPharmacy_03.Xml_Load();
             nodeList.ForEach(item =>
             {
