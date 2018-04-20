5e567fe74702706c42d95320ed03251176a41a5c
Lan Fei <husano896@hotmail.com>
2018-04-20 11:30:41 +0800

    fuk

---
+++ XMLanalysis/App_Data/dbo.桃園公共自行車即時服務資料.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[桃園公共自行車即時服務資料]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [areaId] NCHAR(10) NULL, 
+    [areaName] NCHAR(10) NULL, 
+    [parkName] NCHAR(10) NULL, 
+    [totalSpace] INT NULL, 
+    [surpluSpace] NCHAR(10) NULL, 
+    [payGuide] NCHAR(10) NULL, 
+    [introduction] NCHAR(10) NULL, 
+    [address] NCHAR(10) NULL, 
+    [wgsx] FLOAT NULL, 
+    [wgsy] FLOAT NULL, 
+    [parkId] NCHAR(10) NULL
+	)
---
+++ XMLanalysis/dbo.Table.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [areaId] NCHAR(10) NULL, 
+    [areaName] NCHAR(10) NULL, 
+    [parkName] NCHAR(10) NULL, 
+    [totalSpace] INT NULL, 
+    [surpluSpace] NCHAR(10) NULL, 
+    [payGuide] NCHAR(10) NULL, 
+    [introduction] NCHAR(10) NULL, 
+    [address] NCHAR(10) NULL, 
+    [wgsx] FLOAT NULL, 
+    [wgsy] FLOAT NULL, 
+    [parkId] NCHAR(10) NULL
+	)
