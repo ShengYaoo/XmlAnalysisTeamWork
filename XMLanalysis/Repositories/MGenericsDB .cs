9f61978d43ce5c99a07a0e3dfc2c585be86b5cc8
dell <dell@com>
2018-04-20 09:29:30 +0800

    fix path

---
+++ XMLanalysis/Repositories/MGenericsDB .cs
@@ -0,0 +1,22 @@
+﻿
+using System.Collections.Generic;
+
+
+namespace XMLanalysis
+{
+
+
+    interface MGenericsDB<T>
+    {
+        List<T> Xml_Load();
+        void InsertData(T item);
+        List<T> QueryData(string Row, string Name);
+        void UpdateData(int updateID, T item);
+        void DeleteData(string deleteColumn, string deleteName);
+        void ShowData(List<T> list);
+    }
+
+
+
+
+}
