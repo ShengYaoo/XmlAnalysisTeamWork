7c71a88a326d1e02565ce5af23b04fec3f438aa0
LienChiaLing <1104105312@gm.kuas.edu.tw>
2018-05-03 17:31:51 +0800

    GO

---
+++ XMLanalysis/App_Data/dbo.AQI.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[AQI]
+(
+	[Id] INT NOT NULL , 
+    [County] NVARCHAR(MAX) NULL, 
+    [AQI] INT NULL, 
+    [PM2.5] INT NULL, 
+    [Status] NVARCHAR(MAX) NULL, 
+    [PublishTime] DATETIME NULL, 
+    PRIMARY KEY ([Id])
+)
