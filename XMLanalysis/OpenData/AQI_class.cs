8a58921fd24b0e762d62dbbc62f14588ad0d72f2
LienChiaLing <1104105312@gm.kuas.edu.tw>
2018-05-03 17:42:47 +0800

    class

---
+++ XMLanalysis/OpenData/AQI_class.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XMLanalysis.OpenData
+{
+    class AQI_class
+    {
+        public string County { get; set; }
+        public string AQI { get; set; }
+        public string PM2_5 { get; set; }
+        public string Status { get; set; }
+        public string PublishTime { get; set; }
+    }
+}
